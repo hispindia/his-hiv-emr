e2d54a21e73091cf3c58db9472baa671ec3e0943
Sagar Bele <sagarb.4488@gmail.com>
2015-11-26 10:23:01 +0530

    Added Lab module in system

--- omod/src/main/java/org/openmrs/module/kenyaemr/page/controller/intake/IntakeViewPatientPageController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/page/controller/intake/IntakeViewPatientPageController.java
@@ -14,15 +14,91 @@
 
 package org.openmrs.module.kenyaemr.page.controller.intake;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.openmrs.Encounter;
+import org.openmrs.Form;
+import org.openmrs.Patient;
+import org.openmrs.Program;
+import org.openmrs.Visit;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.appframework.domain.AppDescriptor;
+import org.openmrs.module.kenyacore.form.FormDescriptor;
+import org.openmrs.module.kenyacore.form.FormManager;
+import org.openmrs.module.kenyacore.program.ProgramDescriptor;
 import org.openmrs.module.kenyaemr.EmrConstants;
+import org.openmrs.module.kenyaemr.EmrWebConstants;
+import org.openmrs.module.kenyaui.KenyaUiUtils;
 import org.openmrs.module.kenyaui.annotation.AppPage;
+import org.openmrs.ui.framework.SimpleObject;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.annotation.SpringBean;
+import org.openmrs.ui.framework.page.PageModel;
+import org.openmrs.ui.framework.page.PageRequest;
+import org.openmrs.ui.framework.session.Session;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * View patient page for intake app
  */
 @AppPage(EmrConstants.APP_INTAKE)
 public class IntakeViewPatientPageController {
+	
+	public void controller(@RequestParam(required = false, value = "visitId") Visit visit,
+            @RequestParam(required = false, value = "formUuid") String formUuid,
+            @RequestParam(required = false, value = "section") String section,
+            PageModel model,
+            UiUtils ui,
+            Session session,
+		    PageRequest pageRequest,
+		    @SpringBean KenyaUiUtils kenyaUi,
+		    @SpringBean FormManager formManager) {
+		
+		
+		if ("".equals(formUuid)) {
+			formUuid = null;
+		}
 
-	public void controller() {
+		Patient patient = (Patient) model.getAttribute(EmrWebConstants.MODEL_ATTR_CURRENT_PATIENT);
+
+		AppDescriptor thisApp = kenyaUi.getCurrentApp(pageRequest);
+
+		List<FormDescriptor> oneTimeFormDescriptors = formManager.getCommonFormsForPatient(thisApp, patient);
+		List<SimpleObject> oneTimeForms = new ArrayList<SimpleObject>();
+		for (FormDescriptor formDescriptor : oneTimeFormDescriptors) {
+			Form form = formDescriptor.getTarget();
+			oneTimeForms.add(ui.simplifyObject(form));
+		}
+		model.addAttribute("oneTimeForms", oneTimeForms);
+
+		model.addAttribute("visits", Context.getVisitService().getVisitsByPatient(patient));
+		model.addAttribute("visitsCount", Context.getVisitService().getVisitsByPatient(patient).size());
+		Form form = null;
+		String selection = null;
+		if (visit != null) {
+			selection = "visit-" + visit.getVisitId();
+		}
+		else if (formUuid != null) {
+			selection = "form-" + formUuid;
+			form = Context.getFormService().getFormByUuid(formUuid);
+			List<Encounter> encounters = Context.getEncounterService().getEncounters(patient, null, null, null, Collections.singleton(form), null, null, null, null, false);
+			Encounter encounter = encounters.size() > 0 ? encounters.get(0) : null;
+			model.addAttribute("encounter", encounter);
+		}
+		else {
+			if (StringUtils.isEmpty(section)) {
+				section = "overview";
+			}
+			selection = "section-" + section;
+		}
+
+		model.addAttribute("form", form);
+		model.addAttribute("visit", visit);
+		model.addAttribute("section", section);
+		model.addAttribute("selection", selection);
 	}
 }
--- omod/src/main/webapp/pages/intake/intakeViewPatient.gsp
+++ omod/src/main/webapp/pages/intake/intakeViewPatient.gsp
@@ -2,22 +2,52 @@
 	ui.decorateWith("kenyaemr", "standardPage", [ patient: currentPatient ])
 %>
 
-<div class="ke-page-content">
-	<table cellpadding="0" cellspacing="0" border="0" width="100%">
-		<tr>
-			<td width="40%" valign="top">
-				${ ui.includeFragment("kenyaemr", "patient/patientSummary", [ patient: currentPatient ]) }
-			<!-- 	${ ui.includeFragment("kenyaemr", "patient/patientRelationships", [ patient: currentPatient ]) } -->
-				${ ui.includeFragment("kenyaemr", "program/programHistories", [ patient: currentPatient, showClinicalData: true ]) }
-			</td>
-			<td width="60%" valign="top" style="padding-left: 5px">
-				${ ui.includeFragment("kenyaemr", "visitMenu", [ patient: currentPatient, visit: activeVisit ]) }
+<div class="ke-page-sidebar">
+	<div class="ke-panel-frame">
+		<div class="ke-panel-heading">Visits(${ visitsCount })</div>
 
-				<% if (activeVisit) { %>
-				${ ui.includeFragment("kenyaemr", "visitAvailableForms", [ visit: activeVisit ]) }
-				${ ui.includeFragment("kenyaemr", "visitCompletedForms", [ visit: activeVisit ]) }
+		<% if (!visits) {
+			print ui.includeFragment("kenyaui", "widget/panelMenuItem", [
+				label: ui.message("general.none"),
+			])
+		}
+		else {
+			visits.each { visit ->
+				print ui.includeFragment("kenyaui", "widget/panelMenuItem", [
+						label: ui.format(visit.visitType),
+						href: ui.pageLink("kenyaemr", "intake/intakeViewPatient", [ patientId: currentPatient.id, visitId: visit.id ]),
+						extra: kenyaui.formatVisitDates(visit),
+						active: (selection == "visit-" + visit.id)
+				])
+			}
+		} %>
+	</div>
+
+</div>
+
+<div class="ke-page-content">
+	
+	<% if (visit) { %>
+		<% if (!visit.voided) { %>
+			${ ui.includeFragment("kenyaemr", "visitCompletedForms", [ visit: visit ]) }
+			${ ui.includeFragment("kenyaemr", "visitAvailableForms", [ visit: visit ]) }
+		<% } %>
+
+	<% } else if (form) { %>
+
+		<div class="ke-panel-frame">
+			<div class="ke-panel-heading">${ ui.format(form) }</div>
+			<div class="ke-panel-content">
+
+				<% if (encounter) { %>
+					${ ui.includeFragment("kenyaemr", "form/viewHtmlForm", [ encounter: encounter ]) }
+				<% } else { %>
+					<em>Not filled out</em>
 				<% } %>
-			</td>
-		</tr>
-	</table>
+
+			</div>
+		</div>
+
+	<% } %>
+	
 </div>
