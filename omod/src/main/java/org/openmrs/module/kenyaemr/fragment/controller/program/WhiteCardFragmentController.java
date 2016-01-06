48cfe52e4772c75954ea9ae35e66cd1f16bf0925
Sagar Bele <sagarb.4488@gmail.com>
2016-01-06 16:35:52 +0530

    Part of Patient History - Added print out and some fields for white card and Pre art form - #6604 and #6602

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/WhiteCardFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/WhiteCardFragmentController.java
@@ -1,21 +1,31 @@
 package org.openmrs.module.kenyaemr.fragment.controller.program;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import org.openmrs.Patient;
-import org.openmrs.module.kenyacore.UiResource;
-import org.openmrs.module.kenyacore.program.ProgramDescriptor;
-import org.openmrs.module.kenyacore.program.ProgramManager;
+import org.openmrs.Person;
 import org.openmrs.module.kenyaemr.Dictionary;
-import org.openmrs.module.kenyaemr.EmrWebConstants;
 import org.openmrs.ui.framework.annotation.FragmentParam;
-import org.openmrs.ui.framework.annotation.SpringBean;
 import org.openmrs.ui.framework.fragment.FragmentModel;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class WhiteCardFragmentController {
-	public void controller(FragmentModel model) {
-		model.addAttribute("graphingConcepts", Dictionary.getConcepts(Dictionary.WEIGHT_KG, Dictionary.CD4_COUNT, Dictionary.CD4_PERCENT, Dictionary.HIV_VIRAL_LOAD, Dictionary.CURRENT_WHO_STAGE));
+	public void controller(@RequestParam(value = "patientId", required = false) Person person,@RequestParam(value = "patientId", required = false) Patient patient,
+			FragmentModel model) {
+
+		
+		model.addAttribute("patientAge",patient.getAge());
+		model.addAttribute("patientGender", patient.getGender());
+		model.addAttribute("patientAddress", person.getPersonAddress().getAddress1()+' '+person.getPersonAddress().getCityVillage()+' '+
+											 person.getPersonAddress().getCountyDistrict()+' '+person.getPersonAddress().getStateProvince());
+		
+		model.addAttribute("graphingConcepts", Dictionary.getConcepts(
+				Dictionary.RETURN_VISIT_DATE, Dictionary.WEIGHT_KG,
+				Dictionary.HEIGHT_CM, Dictionary.CURRENT_WHO_STAGE,
+				Dictionary.PREGNANCY_STATUS,
+				Dictionary.EXPECTED_DATE_OF_DELIVERY,
+				Dictionary.METHOD_OF_FAMILY_PLANNING, Dictionary.OI_TB_FORM,
+				Dictionary.MEDICATION_ORDERS, Dictionary.TB_PATIENT,
+				Dictionary.CD4_COUNT, Dictionary.CD4_PERCENT,
+				Dictionary.HIV_VIRAL_LOAD));
+
 	}
 }
--- omod/src/main/webapp/fragments/program/whiteCard.gsp
+++ omod/src/main/webapp/fragments/program/whiteCard.gsp
@@ -4,13 +4,32 @@
 
 <% if (config.complete) { %>
 <div class="ke-stack-item">
-	<table width="100%" border="0">
-		<tr>
-			<td width="50%" valign="top">
-				${ ui.includeFragment("kenyaui", "widget/obsHistoryTable", [ id: "tblhistory", patient: currentPatient, concepts: graphingConcepts ]) }
-			</td>
-		</tr>
-	</table>
+	<div class="widget-content">
+		<table width="100%" border="0">
+			<tr>
+			<td style="text-align: left; vertical-align: top; width: 33%">
+					<strong>${ patientAge }</strong><br/>
+					${ patientGender } <small> ${ patientAddress }</small>
+				</td>
+				
+			</tr>
+			<tr>
+				<td width="50%" valign="top">
+					${ ui.includeFragment("kenyaui", "widget/obsHistoryTable", [ id: "tblhistory", patient: currentPatient, concepts: graphingConcepts ]) }
+				</td>
+			</tr>
+		</table>
+	</div>	
+	<input type="button" onclick="PrintDiv()" value="Print" />
 </div>
 <% } %>
 
+<script type="text/javascript">
+        function PrintDiv() {
+            var divToPrint = document.getElementsByClassName('widget-content')[0];
+            var popupWin = window.open('', '_blank', 'width=300,height=400,location=no,left=200px');
+            popupWin.document.open();
+            popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+            popupWin.document.close();
+        }
+</script>
