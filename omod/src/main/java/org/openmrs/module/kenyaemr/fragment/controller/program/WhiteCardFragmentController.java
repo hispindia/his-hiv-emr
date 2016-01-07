86af4f99b6a598479332e53e967976fb6b3f4757
Sagar Bele <sagarb.4488@gmail.com>
2016-01-07 18:49:28 +0530

    patient Details in White card updated

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/WhiteCardFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/WhiteCardFragmentController.java
@@ -1,8 +1,20 @@
 package org.openmrs.module.kenyaemr.fragment.controller.program;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.openmrs.Concept;
+import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.Person;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.kenyaemr.Dictionary;
+import org.openmrs.module.kenyaemr.wrapper.EncounterWrapper;
+import org.openmrs.module.kenyaemr.wrapper.PatientWrapper;
+import org.openmrs.module.kenyaemr.wrapper.PersonWrapper;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,11 +24,52 @@ public class WhiteCardFragmentController {
 			FragmentModel model) {
 
 		
-		model.addAttribute("patientAge",patient.getAge());
-		model.addAttribute("patientGender", patient.getGender());
-		model.addAttribute("patientAddress", person.getPersonAddress().getAddress1()+' '+person.getPersonAddress().getCityVillage()+' '+
-											 person.getPersonAddress().getCountyDistrict()+' '+person.getPersonAddress().getStateProvince());
+		/*
+		 * Constant value across all visit
+		 * */
+		model.addAttribute("patientName",person.getGivenName());
+		model.addAttribute("patientAge",person.getAge());
+	    model.addAttribute("birthDate", new SimpleDateFormat("dd-MMMM-yyyy").format(person.getBirthdate()));
+		model.addAttribute("patientGender", person.getGender());
+		model.addAttribute("address", person.getPersonAddress());
 		
+		PatientWrapper wrapperPatient = new PatientWrapper(patient);
+		PersonWrapper wrapperPerson = new PersonWrapper(person);
+		
+		model.addAttribute("patientWrap", wrapperPatient);
+		model.addAttribute("personWrap", wrapperPerson);
+		
+		Obs savedEntryPoint = getLatestObs(patient, Dictionary.METHOD_OF_ENROLLMENT);
+		model.addAttribute("savedEntryPoint", savedEntryPoint);
+		if(savedEntryPoint.getValueDate()!=null){
+			model.addAttribute("savedEntryPointValueDate",new SimpleDateFormat("dd-MMMM-yyyy").format(savedEntryPoint.getValueDate()));
+		}
+		else{
+			model.addAttribute("savedEntryPointValueDate","");
+		}
+		
+		/*
+		 * Personal History
+		 * */
+		Obs riskFactor = getAllLatestObs(patient, Dictionary.HIV_RISK_FACTOR);
+		System.out.println(riskFactor.getEncounter());
+		
+		EncounterWrapper wrapped = new EncounterWrapper(riskFactor.getEncounter());
+		List<Obs> obsList = wrapped.allObs(riskFactor.getConcept());
+		String listAllRiskFactor="";
+		for (Obs obs : obsList) {
+			if(listAllRiskFactor.isEmpty()){
+				listAllRiskFactor = listAllRiskFactor.concat(obs.getValueCoded().getName().toString());
+			}
+			else{
+				listAllRiskFactor = listAllRiskFactor.concat(", "+obs.getValueCoded().getName().toString());	
+			}
+		}
+		model.addAttribute("listAllRiskFactor", listAllRiskFactor);
+		
+		/*
+		 * Varaible for each visit
+		 * */
 		model.addAttribute("graphingConcepts", Dictionary.getConcepts(
 				Dictionary.RETURN_VISIT_DATE, Dictionary.WEIGHT_KG,
 				Dictionary.HEIGHT_CM, Dictionary.CURRENT_WHO_STAGE,
@@ -27,5 +80,26 @@ public class WhiteCardFragmentController {
 				Dictionary.CD4_COUNT, Dictionary.CD4_PERCENT,
 				Dictionary.HIV_VIRAL_LOAD));
 
+	}
+	
+	private Obs getLatestObs(Patient patient, String conceptIdentifier) {
+		Concept concept = Dictionary.getConcept(conceptIdentifier);
+		List<Obs> obs = Context.getObsService().getObservationsByPersonAndConcept(patient, concept);
+		if (obs.size() > 0) {
+			// these are in reverse chronological order
+			return obs.get(0);
+		}
+		return null;
+	}
+	
+	private Obs getAllLatestObs(Patient patient, String conceptIdentifier) {
+		Concept concept = Dictionary.getConcept(conceptIdentifier);
+		List<Obs> obs = Context.getObsService().getObservationsByPersonAndConcept(patient, concept);
+		int count=obs.size()-1;
+		if (obs.size() > 0) {
+			// these are in reverse chronological order
+			return obs.get(count);
+		}
+		return null;
 	}
 }
--- omod/src/main/webapp/fragments/program/whiteCard.gsp
+++ omod/src/main/webapp/fragments/program/whiteCard.gsp
@@ -7,15 +7,56 @@
 	<div class="widget-content">
 		<table width="100%" border="0">
 			<tr>
-			<td style="text-align: left; vertical-align: top; width: 33%">
-					<strong>${ patientAge }</strong><br/>
-					${ patientGender } <small> ${ patientAddress }</small>
+				<td>
+					<center>Patient Identification</center> 
 				</td>
-				
 			</tr>
 			<tr>
+			<td style="text-align: left; vertical-align: top; width: 33%">
+					<br/><strong>Name : ${ patientName }</strong>
+					<br/><strong>Age : ${ patientAge }</strong>
+					<strong>Date of Birth : ${ birthDate }</strong>
+					<br/><strong>Gender : ${ patientGender } </strong>
+					<br/><strong>Address : </strong><small> ${ address.address1}.</small>
+					<% if (personWrap.telephoneContact) { %>
+						<strong>Patient's phone number : </strong><small> ${personWrap.telephoneContact}.</small>
+					<% } %>
+					<br/><strong>Village/City : </strong><small> ${ address.cityVillage}.</small>
+					<strong>Township : </strong><small> ${ address.countyDistrict}.</small>
+					<strong>State or Region : </strong><small> ${ address.stateProvince}.</small>
+					<br/>
+					<% if (patientWrap.nextOfKinName) { %>
+						<strong>Treatment Supporter's Name: </strong><small>${patientWrap.nextOfKinName}</small>
+					<% } %>
+					<% if (patientWrap.nextOfKinAddress) { %>
+						<strong>Treatment Supporter's Address: </strong><small>${patientWrap.nextOfKinAddress}</small>
+					<% } %>
+					<% if (patientWrap.nextOfKinContact) { %>
+						<strong>Treatment Supporter's phone number: </strong><small>${patientWrap.nextOfKinContact}</small>
+					<% } %>
+					
+					<br/><strong>Date HIV+ test: </strong><small>${patientWrap.previousHivTestDate}</small> 
+					<strong>Plcae: </strong><small>${patientWrap.previousHivTestPlace}</small>
+					<br/>	<strong>Entry Point : </strong><small>${savedEntryPoint.valueCoded.name}</small>
+					<br/>
+					<% if (savedEntryPointValueDate) { %>
+						<strong>Name Previous Clinic: </strong><small>${savedEntryPoint.valueText}</small>
+					<strong>Date Transferred in: </strong><small>${savedEntryPointValueDate}</small>
+					<% } %>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<br/><center>Personal History</center> 
+				</td>
+			<tr>
+			<tr>
+				<td>
+					${listAllRiskFactor}
+				</td>
+			</tr>
 				<td width="50%" valign="top">
-					${ ui.includeFragment("kenyaui", "widget/obsHistoryTable", [ id: "tblhistory", patient: currentPatient, concepts: graphingConcepts ]) }
+					<br/>${ ui.includeFragment("kenyaui", "widget/obsHistoryTable", [ id: "tblhistory", patient: currentPatient, concepts: graphingConcepts ]) }
 				</td>
 			</tr>
 		</table>
