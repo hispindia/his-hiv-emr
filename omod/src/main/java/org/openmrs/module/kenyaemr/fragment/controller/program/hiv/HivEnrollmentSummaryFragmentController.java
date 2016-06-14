0c8c24a3fa378773d88e891f5ba2ee12b23693b9
Sagar Bele <sagarb.4488@gmail.com>
2016-06-14 16:16:12 +0530

    #8332 , #8353

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/hiv/HivEnrollmentSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/hiv/HivEnrollmentSummaryFragmentController.java
@@ -14,6 +14,7 @@
 
 package org.openmrs.module.kenyaemr.fragment.controller.program.hiv;
 
+import org.apache.commons.collections.iterators.ArrayListIterator;
 import org.openmrs.Concept;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
@@ -27,7 +28,9 @@ import org.openmrs.module.kenyaemr.wrapper.Enrollment;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,57 +42,59 @@ public class HivEnrollmentSummaryFragmentController {
 	private Obs savedEntryPoint;
 	private String otherEntryPoint;
 	private String entryPoint;
-	public String controller(@FragmentParam("patientProgram") PatientProgram patientProgram,
-						   @FragmentParam(value = "encounter", required = false) Encounter encounter,
-						   @FragmentParam("showClinicalData") boolean showClinicalData,
-						   FragmentModel model) {
-		
+
+	public String controller(
+			@FragmentParam("patientProgram") PatientProgram patientProgram,
+			@FragmentParam(value = "encounter", required = false) Encounter encounter,
+			@FragmentParam("showClinicalData") boolean showClinicalData,
+			FragmentModel model) {
+
 		Map<String, Object> dataPoints = new LinkedHashMap<String, Object>();
-		
+
 		dataPoints.put("Enrolled Date", patientProgram.getDateEnrolled());
-		
+
 		Patient patient = patientProgram.getPatient();
-		savedEntryPoint = getLatestObs(patient, Dictionary.METHOD_OF_ENROLLMENT);
+
+		savedEntryPoint = getLatestObs(patient, encounter,
+				Dictionary.METHOD_OF_ENROLLMENT);
 		if (savedEntryPoint != null) {
 			entryPoint = savedEntryPoint.getValueCoded().getName().toString();
 			otherEntryPoint = savedEntryPoint.getValueText();
-			if(otherEntryPoint!=null){
-			dataPoints.put("Entry Point", entryPoint+" " +"("+otherEntryPoint+")");
-			}
-			else {
-				dataPoints.put("Entry Point", entryPoint );
-			}
-		}
-/*		
-		if (encounter != null) {
-			EncounterWrapper wrapper = new EncounterWrapper(encounter);
-
-			Obs o = wrapper.firstObs(Dictionary.getConcept(Dictionary.METHOD_OF_ENROLLMENT));
-			if (o != null) {
-				dataPoints.put("Entry point", o.getValueCoded());
+			if (otherEntryPoint != null) {
+				dataPoints.put("Entry Point", entryPoint + " " + "("
+						+ otherEntryPoint + ")");
+			} else {
+				dataPoints.put("Entry Point", entryPoint);
 			}
 		}
 
-		if (showClinicalData) {
-			Enrollment enrollment = new Enrollment(patientProgram);
-
-			Obs o = enrollment.firstObs(Dictionary.getConcept(Dictionary.CURRENT_WHO_STAGE));
-			if (o != null) {
-				dataPoints.put("WHO stage", o.getValueCoded());
-			}
-		}
-*/
 		model.put("dataPoints", dataPoints);
 		return "view/dataPoints";
 	}
-	
-	private Obs getLatestObs(Patient patient, String conceptIdentifier) {
+
+	private Obs getLatestObs(Patient patient, Encounter en,
+			String conceptIdentifier) {
 		Concept concept = Dictionary.getConcept(conceptIdentifier);
-		List<Obs> obs = Context.getObsService().getObservationsByPersonAndConcept(patient, concept);
-		if (obs.size() > 0) {
-			// these are in reverse chronological order
-			return obs.get(0);
+		List<Obs> obs = Context.getObsService()
+				.getObservationsByPersonAndConcept(patient, concept);
+
+		List<Encounter> eList = Context.getEncounterService()
+				.getEncountersByPatient(patient);
+		List<Encounter> enFormList = new ArrayList<Encounter>();
+		for (Encounter e : eList) {
+			if (e.getForm().getUuid()
+					.endsWith("e4b506c1-7379-42b6-a374-284469cba8da")) {
+				enFormList.add(e);
+			}
 		}
+		for (Encounter ef : enFormList) {
+			for (Obs o : obs) {
+				if (en.getEncounterDatetime().after(o.getObsDatetime())) {
+					return o;
+				}
+			}
+		}
+
 		return null;
 	}
 }
